8e3814cae1b66c3bef7355c4fd1a45ae61b47739
dqh19930227 <ding_qinghui@163.com>
2020-05-07 14:39:53 +0800

    删除文件

--- include/linux/version.h
+++
@@ -1,8 +0,0 @@
-#define LINUX_VERSION_CODE 199168
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
-#define RHEL_MAJOR 7
-#define RHEL_MINOR 7
-#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
-#define RHEL_RELEASE_CODE 1799
-#define RHEL_RELEASE "1062.4.3"
-#define KERNEL_HEADERS_CHECKSUM "b4da4d7455cb48c5d3ba707e345f89b72dae4990"
